f77e5e9e213f363d71ca5db07b6fa464d09837b6
Johknotter Hill <johknotterhill@Johknotters-MacBook-Pro.local>
2020-04-29 20:23:58 -0400

    Added axios for http requests to add to my thunk actionCreators

--- warbler-client/src/services/api.js
+++ warbler-client/src/services/api.js
@@ -8,12 +8,17 @@ export function setTokenHeader(token) {
     delete axios.defaults.headers.common["Authorization"];
   }
 }
+
+
 /**
 * A wrapper around axios API call that formats errors, etc
 * @params {string} method - the HTTP verb you want to use
 * @params {string} path - the route path / endpoint
 * @params {object} data - (optional) data in JSON form for POST requests
 */
+
+
+
 export function apiCall(method, path, data) {
   return new Promise((resolve, reject) => {
     return axios[method](path, data)
